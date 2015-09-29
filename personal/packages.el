8a75f8655e02699bfbe0445956c2aeb8eaeb5fe2
Fedor Baart <fedor.baart@deltares.nl>
2015-09-29 21:40:35 +0200

    packages

---
+++ personal/packages.el
@@ -0,0 +1,72 @@
+(prelude-require-packages '(
+                            multiple-cursors
+                            ;; html support
+                            company-web
+                            ;; support both autocomplete and company
+                            ac-js2             ; js2 completion
+                            ac-html-bootstrap                  ; bootstrap completions
+                            ac-html-csswatcher                 ; css completions
+
+                            ;; Ido
+                            ido-at-point
+                            ido-vertical-mode                  ; shows ido options in a vertical list
+                            ;;
+                            switch-window                      ; window suggestion
+
+
+                            ;; Snippets
+                            angular-snippets                   ; snippets
+
+
+                            visual-regexp
+                            w3m                                ; browse
+                            whitespace-cleanup-mode
+                            ;; modes
+
+                            ;; Mako
+                            mmm-mode
+                            mmm-mako                           ; Mako templates
+                            mmm-jinja2                         ; Jinja2 templates
+                            ;; ansible
+                            ansible                            ; ansible
+                            ansible-doc                        ; ansible docs
+                            ;; clojure
+                            clojure-mode
+                            cider
+                            ;; general lisp
+                            paredit                            ; for lisp
+                            ;; Python
+                            virtualenv                         ; python virtual envs
+                            ein                                ; ipython notebook
+                            ;; SQLite
+                            esqlite                            ; sqlite
+                            ;; R
+                            ess                                ; for R
+                            r-autoyas                          ; r extra
+                            ;; Rest
+                            restclient
+                            ;; Ruby
+                            rvm                                ; Default ruby
+
+                            ;; Fortran
+                            fortpy                             ; fortran auto complete
+                            ;; Shaders
+                            glsl-mode                          ; shaders
+                            ;; Python
+                            jedi                               ; auto complete in emacs
+                            ;; Latex
+                            latex-preview-pane                 ; preview latex
+                            ;; Nodejs
+                            nodejs-repl
+
+                            ;; Themes
+                            solarized-theme                    ; theme
+
+                            ;; Doc
+                            css-eldoc
+                            ;; Keyboard
+                            guide-key                          ; overview of key chords for C x r
+
+
+
+))
